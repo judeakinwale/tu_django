cdf4d821a75242b2cfd2398fe8c077d88b752984
jude akinwale <judeakinwale@gmail.com>
2021-03-02 19:32:56 +0100

    Add first name and last name to the registration form

--- registration/forms.py
+++ registration/forms.py
@@ -7,10 +7,12 @@ class NewUserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
