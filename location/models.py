12c71f4b1774013f9ae2b6af2d09802b920d7829
jude akinwale <judeakinwale@gmail.com>
2021-03-02 19:27:46 +0100

    Add listing category and add timestamp to the models

--- location/models.py
+++ location/models.py
@@ -1,52 +1,71 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from django.shortcuts import reverse
 from datetime import datetime
+from core.models import EventCity, EventState
 
 # Create your models here.
 
 class Listing(models.Model):
-  realtor = models.ForeignKey("Realtor", on_delete=models.DO_NOTHING)
-  title = models.CharField(max_length=300)
-  description = models.TextField(blank=True, null=True)
-  address = models.CharField(max_length=300)
-  city = models.CharField(max_length=200)
-  state = models.CharField(max_length=200)
-  country = models.CharField(max_length=200)
-  zipcode = models.CharField(max_length=100, blank=True, null=True)
-  price = models.DecimalField(max_digits=15, decimal_places=2)
-  bedrooms = models.IntegerField()
-  bathrooms = models.DecimalField(max_digits=10, decimal_places=1)
-  pools = models.IntegerField()
-  lot_size = models.DecimalField(max_digits=10, decimal_places=1, blank=True, null=True)
-  photo_main = models.ImageField(upload_to='images/%Y/%m/%d/')
-  photo_1 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
-  photo_2 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
-  photo_3 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
-  photo_4 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
-  photo_5 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
-  photo_6 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
-  list_date = models.DateTimeField(default=datetime.now)
-  is_published = models.BooleanField(default=True)
-  is_booked = models.BooleanField(default=False)
+    realtor = models.ForeignKey("Realtor", on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=300)
+    description = models.TextField(blank=True, null=True)
+    address = models.CharField(max_length=300)
+    city = models.ForeignKey(EventCity, on_delete=models.SET_NULL, blank=True, null=True)
+    state = models.ForeignKey(EventState, on_delete=models.SET_NULL, blank=True, null=True)
+    country = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=100, blank=True, null=True)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=10, decimal_places=1)
+    pools = models.IntegerField()
+    lot_size = models.DecimalField(max_digits=10, decimal_places=1, blank=True, null=True)
+    photo_main = models.ImageField(upload_to='images/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
+    photo_2 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
+    photo_3 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
+    photo_4 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
+    photo_5 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
+    photo_6 = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
+    list_date = models.DateTimeField(default=datetime.now)
+    # timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
+    updated = models.DateTimeField(auto_now=False, auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+    is_booked = models.BooleanField(default=False)
 
-  def __str__(self):
-      return self.title
+    def __str__(self):
+        return self.title
 
-  def get_absolute_url(self):
-      return reverse("location:location_detail", kwargs={"pk": self.pk})
+    def get_absolute_url(self):
+        return reverse("location:location_detail", kwargs={"pk": self.pk})
+
+
+class ListingCategory(models.Model):
+    name = models.CharField(max_length=200)
+    summary = models.TextField(blank=True, null=True)
+    # slug = models.SlugField(unique=True)
+    timestamp = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = "Category"
+
+    verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
 
 
 class Realtor(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  name =  models.CharField(max_length=200, blank=True, null=True)
-  photo = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
-  description = models.TextField(blank=True, null=True)
-  phone = models.CharField(max_length=30)
-  email = models.CharField(max_length=50)
-  is_trusted = models.BooleanField(default=False)
-  join_date = models.DateTimeField(default=datetime.now, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name =  models.CharField(max_length=200, blank=True, null=True)
+    photo = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    phone = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    is_trusted = models.BooleanField(default=False)
+    join_date = models.DateTimeField(default=datetime.now, blank=True, null=True)
+    # timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
+    updated = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-  def __str__(self):
-      return self.name
-  
+    def __str__(self):
+        return self.name
--- location/views.py
+++ location/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
-from .models import Listing, Realtor
+from .models import Listing, Realtor, ListingCategory
+from core.models import EventCity, EventState
 
 # Create your views here.
 
@@ -12,6 +13,47 @@ def search(request):
         context = {'query': query, 'search_result': listing}
     except:
         context = {'query': query, 'search_list': 'Sorry, that event does not exist or has take place'}
+    return render(request, template_name, context)
+
+
+def search(request):
+    queryset_list = Listing.objects.order_by('-timestamp')
+
+    # Keywords
+    if 'query' in request.GET:
+        query = request.GET['query']
+        if query:
+            queryset_list = queryset_list.filter(name__icontains=query)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            city_id = EventCity.objects.get(name__iexact=city).id
+            queryset_list = queryset_list.filter(city=city_id)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            state_id = EventState.objects.get(name__iexact=state).id
+            queryset_list = queryset_list.filter(state=state_id)
+
+    # Category
+    if 'category' in request.GET:
+        category = request.GET['category']
+        if category:
+            category_id = ListingCategory.objects.get(name__iexact=category).id
+            queryset_list = queryset_list.filter(category=category_id)
+
+    template_name = 'core/search_list.html'
+    context = {
+        'query': request.GET['query'],
+        'search_list': queryset_list,
+        'categories': EventCategory.objects.all(),
+        'cities': EventCity.objects.all(),
+        'states': EventState.objects.all(),
+    }
     return render(request, template_name, context)
 
 
