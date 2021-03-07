0903461f696afedc76d7ae8a8a525bb08c2934a2
jude akinwale <judeakinwale@gmail.com>
2021-03-07 19:53:43 +0100

    Improve html templates, add contact us model to the registration app

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactUs(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.CharField(max_length=250)
+    message =  models.TextField()
+    timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    class Meta:
+        verbose_name = "ContactUs"
+        verbose_name_plural = "ContactUs"
+
+    def __str__(self):
+        return f"{self.first_name} {self.first_name} - Contact Query"
+
